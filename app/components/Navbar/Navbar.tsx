040b40ed3df946f4e4e95ce81cc37afcdb715d82
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-01-22 23:53:35 +0100

    contd

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -10,9 +10,8 @@ const Navbar = () => {
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
-                    <div 
-                    className="flex flex-row items-center justify-between gap-3 md:gap-0"
-                    >
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                     
                      <Logo />
 
                      <Search />   
---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useCallback, useEffect, useState } from "react";
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryLabel
+}) => {
+    const [showModal, setShowModal] = useState(isOpen);
+
+    useEffect(() => {
+        setShowModal(isOpen)
+    }, [isOpen]);
+
+    const handleClose = useCallback(() => {
+        if (disabled) {
+            return;
+        }
+
+        setShowModal(false);
+        setTimeout(() => {
+            onClose();
+        }, 300);
+    }, [disabled, onClose]); 
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Modal;
